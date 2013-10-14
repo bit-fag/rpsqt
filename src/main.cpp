eb203c1bef385fe3e60dff5d9dc21bb9dabeed09
Bitcoin Fan <ali.virod@yandex.ru>
2013-10-15 03:43:47 +0400

    Initialize qrand() by qsrand()

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <QApplication>
+#include <QTime>
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
+    QTime time = QTime::currentTime();
+    qsrand((uint)time.msec());
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
