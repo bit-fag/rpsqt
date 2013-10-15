0a77deb7477f865ef1715d607d5b6ed5454d8895
Bitcoin Fan <ali.virod@yandex.ru>
2013-10-15 13:21:51 +0400

    Translate into Russian

--- locale/rpsqt_ru.ts
+++ locale/rpsqt_ru.ts
@@ -6,80 +6,80 @@
     <message>
         <location filename="../ui/mainwindow.ui" line="20"/>
         <source>Rock-paper-scissors</source>
-        <translation type="unfinished">Камень-ножницы-бумага</translation>
+        <translation>Камень-ножницы-бумага</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="33"/>
         <source>Rock</source>
-        <translation type="unfinished">Камень</translation>
+        <translation>Камень</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="44"/>
         <source>Paper</source>
-        <translation type="unfinished">Бумага</translation>
+        <translation>Бумага</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="55"/>
         <source>Scissors</source>
-        <translation type="unfinished">Ножницы</translation>
+        <translation>Ножницы</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="66"/>
         <source>Random</source>
-        <translation type="unfinished">Наугад</translation>
+        <translation>Наугад</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="80"/>
         <location filename="../ui/mainwindow.ui" line="94"/>
         <source>Enter what your opponent sent:</source>
-        <translation type="unfinished">Введите текст, присланный соперником:</translation>
+        <translation>Введите текст, присланный соперником:</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="148"/>
         <location filename="../ui/mainwindow.ui" line="179"/>
         <source>Copy</source>
-        <translation type="unfinished"></translation>
+        <translation>Копировать</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="128"/>
         <location filename="../ui/mainwindow.ui" line="159"/>
         <source>Send this:</source>
-        <translation type="unfinished"></translation>
+        <translation>Отправьте это:</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="190"/>
         <source>Result</source>
-        <translation type="unfinished"></translation>
+        <translation>Результат</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="197"/>
         <source>New game</source>
-        <translation type="unfinished"></translation>
+        <translation>Новая игра</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="189"/>
         <source>Draw: </source>
-        <translation type="unfinished"></translation>
+        <translation>Ничья:</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="192"/>
         <source>You won: </source>
-        <translation type="unfinished"></translation>
+        <translation>Вы победили:</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="195"/>
         <source>Opponent won: </source>
-        <translation type="unfinished"></translation>
+        <translation>Соперник победил:</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="199"/>
         <source>Invalid choice of your opponent! Although he doesn&apos;t cheat</source>
-        <translation type="unfinished"></translation>
+        <translation>Выбор соперника недействителен! Но не похоже, что он пытался сжульничать</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="203"/>
         <source>Opponent hash FAIL!!! It looks like he cheats</source>
-        <translation type="unfinished"></translation>
+        <translation>Хеш соперника НЕ СХОДИТСЯ!!! Кажется, он жульничает</translation>
     </message>
 </context>
 <context>
@@ -87,22 +87,22 @@
     <message>
         <location filename="../src/mainwindow.cpp" line="138"/>
         <source>rock</source>
-        <translation type="unfinished"></translation>
+        <translation>камень</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="140"/>
         <source>paper</source>
-        <translation type="unfinished"></translation>
+        <translation>бумага</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="142"/>
         <source>scissors</source>
-        <translation type="unfinished"></translation>
+        <translation>ножницы</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="144"/>
         <source>error</source>
-        <translation type="unfinished"></translation>
+        <translation>ошибка</translation>
     </message>
 </context>
 </TS>
